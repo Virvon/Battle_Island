e9c390eaa44b9a7d432d81d0a4fb40d127307802
Virvon <dan130205@gmail.com>
2023-05-17 13:08:57 +0300

    Add Game Init

---
+++ Assets/Sources/GameInit.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using UnityEngine;
+using Agava.YandexGames;
+
+public class GameInit : MonoBehaviour
+{
+    private void Awake()
+    {
+        YandexGamesSdk.CallbackLogging = true;
+    }
+
+    private IEnumerator Start()
+    {
+#if !UNITY_WEBGL || UNITY_EDITOR
+        yield break;
+#endif
+
+        yield return YandexGamesSdk.Initialize();
+    }
+}
