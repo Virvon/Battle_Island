142129e2b41ca22d42e59aab5462ff6fd7c8598f
Virvon <dan130205@gmail.com>
2023-05-18 13:58:39 +0300

    Add GameInit

--- Assets/Sources/GameInit.cs
+++ Assets/Sources/GameInit.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using UnityEngine;
 using Agava.YandexGames;
+using System;
 
 public class GameInit : MonoBehaviour
 {
+    [SerializeField] private string _startScene;
+
+    private SceneLoader _sceneLoader;
+
     private void Awake()
     {
         YandexGamesSdk.CallbackLogging = true;
+
+        _sceneLoader = new SceneLoader(_startScene);
     }
 
     private IEnumerator Start()
@@ -16,5 +23,11 @@ public class GameInit : MonoBehaviour
 #endif
 
         yield return YandexGamesSdk.Initialize();
+
+        if (YandexGamesSdk.IsInitialized == false)
+            throw new ArgumentNullException(nameof(YandexGamesSdk), "Yandex SDK didn't initialized correctly");
+
+
+        _sceneLoader.Load();
     }
 }
